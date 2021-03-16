eb5fb402c4a5b5e4d139327271518a0e7e4e5a9b
Callum <callumhewitt99@gmail.com>
2021-03-16 04:05:55 +0000

    lab6 part1

--- server/server.js
+++ server/server.js
@@ -2,12 +2,24 @@ import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://callum:Callum99@cluster0.1s8nb.mongodb.net/WebDevCW?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 // Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, dbName: "users"})
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, dbName: "WebDevCW"})
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
+
+
+
 
 app.listen(config.port, (err) => {
   if (err) {
