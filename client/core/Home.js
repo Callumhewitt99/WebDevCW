1a154d48d53031aa6c9b26828c12fb226349fc9d
Callum <callumhewitt99@gmail.com>
2021-03-18 15:36:34 +0000

    admin page not working dad jokes api working

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,11 +1,13 @@
-import React from 'react'
+
+import React, {useState, useEffect} from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import myImg from './../assets/images/dylan.jpg'
+import myImg from './../assets/images/myimage.png'
 import {Link} from 'react-router-dom'
+import {joke} from '../thridparty/api-dadjokes.js'
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -34,6 +36,35 @@ const useStyles = makeStyles(theme => ({
 
 export default function Home(){
   const classes = useStyles()
+  const [jokes, setJokes] = useState({
+    joke: 'No joke',
+    error: ''
+  })
+  useEffect(() => {
+    const abortController = new AbortController()
+    const signal = abortController.signal
+
+    joke(signal).then((data) => {
+      if (data && data.error) {
+        console.log("error in getting jokes")
+        console.log(data.error)
+        //setJokes(...jokes, error: data.error)
+      } else {
+      	console.log("Here is the user data")
+      	console.log(data)
+      	if (data != undefined){
+      		console.log("setting the data")
+        	setJokes(data)
+        }
+      }
+    })
+
+    return function cleanup(){
+      abortController.abort()
+    }
+  }, [])    
+    
+    
     return (
         <Card className={classes.card}>
           <Typography variant="h6" className={classes.title}>
@@ -44,8 +75,10 @@ export default function Home(){
           <Typography variant="body2" component="p" className={classes.credit} color="textSecondary"> Image by Picasso </Typography>
           <CardContent>
             <Typography variant="body1" component="p">
-              Welcome to Lab Six.
+              Welcome to Lab Six again.
+              {jokes.joke}
             <Link to="/users"> Users </Link>
+            
             </Typography>
           </CardContent>
         </Card>
